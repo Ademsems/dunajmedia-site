9971523662f76fd625d9188dbf8aabf2fb571dd2
Ademsems <140335883+Ademsems@users.noreply.github.com>
2026-03-04 14:06:30 +0100

    Add files via upload

---
+++ src/pricing/page.tsx
@@ -0,0 +1,9 @@
+import PricingTable from "@/components/sections/PricingTable";
+
+export default function PricingPage() {
+  return (
+    <main className="pt-20">
+      <PricingTable />
+    </main>
+  );
+}
