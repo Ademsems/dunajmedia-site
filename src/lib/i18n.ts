f3c56320f79576648d671acad0a08ab44fdb9c2f
Ademsems <140335883+Ademsems@users.noreply.github.com>
2026-03-04 13:54:39 +0100

    Update i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,16 +2,12 @@ import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
 export const locales = ['en', 'sk'] as const;
-export type Locale = (typeof locales)[number];
-export const defaultLocale: Locale = 'en';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    locale, // This must be here
-    messages: (await import(`../messages/${locale}.json`)).default,
-    timeZone: 'Europe/Bratislava',
-    now: new Date()
+    locale, // CRITICAL: This must be here
+    messages: (await import(`../messages/${locale}.json`)).default
   };
 });
