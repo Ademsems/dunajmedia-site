1e53ffdb5ae93beaceeceaec62841320538b9e70
Ademsems <140335883+Ademsems@users.noreply.github.com>
2026-03-04 13:30:30 +0100

    Update i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,18 +3,14 @@ import { getRequestConfig } from 'next-intl/server';
 
 export const locales = ['en', 'sk'] as const;
 export type Locale = (typeof locales)[number];
-// We added 'export' here so the middleware can see it
 export const defaultLocale: Locale = 'en';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
-
-  if (!locale || !locales.includes(locale as Locale)) {
-    locale = defaultLocale;
-  }
+export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming locale is supported
+  if (!locales.includes(locale as any)) notFound();
 
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages: (await import(`../messages/${locale}.json`)).default
   };
 });
