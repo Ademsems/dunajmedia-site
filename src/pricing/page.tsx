5747c23a8cedac7843617b24dd060c2e14927ed6
Ademsems <140335883+Ademsems@users.noreply.github.com>
2026-03-04 15:42:37 +0100

    Delete src/pricing directory

--- src/pricing/page.tsx
+++
@@ -1,9 +0,0 @@
-import PricingTable from "@/components/sections/PricingTable";
-
-export default function PricingPage() {
-  return (
-    <main className="pt-20">
-      <PricingTable />
-    </main>
-  );
-}
